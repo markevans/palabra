a342aebd07402cb75bb3df9e4524e45fdaa0bb7f
Mark Evans <mark@new-bamboo.co.uk>
2013-11-20 13:22:51 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Palabra::Application.config.secret_key_base = '390a64c77f8cbd35bc07fd7809fa653e6ff1fb4677e7d6d0752739d709f8034de676fbb41f9561dc2f31aee1b61f896797c891ab1eb7774ec0562e9439ecb141'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Palabra::Application.config.session_store :cookie_store, key: '_palabra_session'
